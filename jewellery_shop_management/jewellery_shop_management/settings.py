6acd41c911f7bb962ff7b5d162882fc5b8c09cf4
Justina Zymonaite <jzymonaite@yahoo.com>
2022-11-29 14:26:08 +0200

    sukurtas local_settings.py failas ir pakoreguotas secret_key settingsuose

--- jewellery_shop_management/jewellery_shop_management/settings.py
+++ jewellery_shop_management/jewellery_shop_management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from . import local_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d!afti%e=7b*b0-r)rky@f9jy+bb!x38!t&@l&ptmc__+dxrwy'
+SECRET_KEY = local_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
